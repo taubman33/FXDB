d948743b348aa143520b2b4135ccf9f0e23ad72f
jeremy taubman <jeremytaubman@jeremys-MacBook-Air.local>
2020-03-09 16:33:00 -0400

    Search bar function

--- effects-app/config/routes.rb
+++ effects-app/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :users
-resources :pedals
+resources :pedals do 
+  collection do
+    get 
 
 end
