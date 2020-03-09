ebd95035c5bb09c2b4fe443d1676a58289a0c657
jeremy taubman <jeremytaubman@jeremys-MacBook-Air.local>
2020-03-09 10:59:18 -0400

    models made

---
+++ project-app/app/models/pedal.rb
@@ -0,0 +1,2 @@
+class Pedal < ApplicationRecord
+end
---
+++ project-app/db/migrate/20200309145853_create_pedals.rb
@@ -0,0 +1,17 @@
+class CreatePedals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pedals do |t|
+      t.string :name
+      t.string :maker
+      t.string :type
+      t.string :subtype
+      t.string :notable_users
+      t.string :songs_used_on
+      t.string :photo
+      t.user :references
+      t.song :references
+
+      t.timestamps
+    end
+  end
+end
