7cc78a330a72538447f043fd068cadeadeb3011b
jeremy taubman <jeremytaubman@jeremys-MacBook-Air.local>
2020-03-10 17:08:07 -0400

    reseeded db added some styling now to decompress before I explode

--- effects-app/app/models/user.rb
+++ effects-app/app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 2 }
+ 
 end
--- effects-app/db/seeds.rb
+++ effects-app/db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: 'Git Tarest', email: 'email@email.com', password_digest: 'password', active: true)
-User.create!(username: 'Basega Tarest', email: 'email1@email.com', password_digest: 'password', active: true)
-User.create!(username: 'Thad Rummer', email: 'email2@email.com', password_digest: 'password', active: false)
+User.create!(username: 'Git Tarestt', email: 'email23@email.com', password_digest: 'password', active: true)
+User.create!(username: 'Basega Tarestt', email: 'email21@email.com', password_digest: 'password', active: true)
+User.create!(username: 'Thad Rummerr', email: 'email22@email.com', password_digest: 'password', active: false)
 puts "#{User.count} users created!"
 
 @user = User.create!(username: 'Git Tarest', email: 'email@email.com', password_digest: 'password', active: true)
-@pedal1 = @user.pedals.build(name:'Big Muff', maker:'Electro Harmonix', effect_type:'dirt', effect_subtype:'fuzz', notable_users:'Dan Auerbach (The Black Keys)', songs_used_on: 'The Black Keys - Thickfreakness', photo:'https://i.imgur.com/BTIbW6M.jpg')                                                                                                                                 
+@pedal1 = @user.pedals.build(name:'Big Muff', maker:'Electro Harmonix', effect_type:'dirt', effect_subtype:'Fuzz', notable_users:'Dan Auerbach (The Black Keys)', songs_used_on: 'The Black Keys - Thickfreakness', photo:'https://i.imgur.com/BTIbW6M.jpg')                                                                                                                                 
 @pedal1.save                                                                                                                              
-@pedal2 = @user.pedals.build(name:'Tube Screamer', maker:'Ibanez', effect_type:'dirt', effect_subtype:'overdrive', notable_users:'Eric Johnson', songs_used_on:'Stevie Ray Vaughn - Pride and Joy', photo: 'https://i.imgur.com/2UfzxG2.jpg')
+@pedal2 = @user.pedals.build(name:'Tube Screamer', maker:'Ibanez', effect_type:'dirt', effect_subtype:'Overdrive', notable_users:'Eric Johnson', songs_used_on:'Stevie Ray Vaughn - Pride and Joy', photo: 'https://i.imgur.com/2UfzxG2.jpg')
 @pedal2.save
-@pedal3 = @user.pedals.build(name:'Centaur', maker:'Klon', effect_type:'dirt', effect_subtype:'overdrive', notable_users:'John Mayer', songs_used_on: 'John Mayer - Slow Dancing In a Room on Fire', photo:'https://i.imgur.com/j4fwi0D.jpg')
+@pedal3 = @user.pedals.build(name:'Centaur', maker:'Klon', effect_type:'dirt', effect_subtype:'Overdrive', notable_users:'John Mayer', songs_used_on: 'John Mayer - Slow Dancing In a Room on Fire', photo:'https://i.imgur.com/j4fwi0D.jpg')
 @pedal3.save
 @pedal4 = @user.pedals.build(name:'Rat', maker:'ProCo', effect_type:'dirt', effect_subtype:'Distortion', notable_users:'Jeff Beck',  songs_used_on:'none', photo: 'https://i.imgur.com/3g17Rrl.jpg')
 @pedal4.save
@@ -28,7 +28,7 @@ puts "#{User.count} users created!"
 @pedal7.save
 @pedal8 = @user.pedals.build(name:'Blue Box', maker:'MXR', effect_type:'dirt', effect_subtype:'Fuzz', notable_users:'Gary Clark Jr.',  songs_used_on:'Gary Clark Jr. - Bright Lights Big City', photo: 'https://i.imgur.com/e3IOg2S.jpg')
 @pedal8.save
-@pedal9 = @user.pedals.build(name:'CryBaby', maker:'Dunlop', effect_type:'pitch', effect_subtype:'Wah', notable_users:'Slash',  songs_used_on:'Guns N Roses - Paradise City', photo: 'https://i.imgur.com/4cwbJSQ.jpg')
+@pedal9 = @user.pedals.build(name:'CryBaby', maker:'Dunlop', effect_type:'pitch', effect_subtype:'Wah', notable_users:'Jimi Hendrix, Slash (Guns N Roses), Kirk Hammett (Metallica)',  songs_used_on:'Guns N Roses - Paradise City', photo: 'https://i.imgur.com/4cwbJSQ.jpg')
 @pedal9.save
 @pedal10 = @user.pedals.build(name:'Fuzz Face', maker:'Dallas-Arbiter', effect_type:'dirt', effect_subtype:'Fuzz', notable_users:'Jimi Hendrix',  songs_used_on:'Jimi Hendrix - All Along the Watchtower', photo: 'https://i.imgur.com/Cr3cghz.jpg')
 @pedal10.save
@@ -42,17 +42,4 @@ Song.create!(name:'Satisfaction', artist:'The Rolling Stones', guitarist:'Keith 
 Song.create!(name:'Seven Nation Army', artist:'The White Stripes', guitarist:'Jack White', pedals_used:'Electro Harmonix Big Muff')
 Song.create!(name:'Time', artist:'Pink Floyd', guitarist:'David Gilmour', pedals_used:'Dallas Arbiter Fuzz Face')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 puts "#{Song.count} songs created!"
-
-
-
-
-
-=======
-puts "#{Song.count} songs created!"
->>>>>>> parent of 55cfbec... tuesday morning
-=======
-puts "#{Song.count} songs created!"
->>>>>>> parent of 55cfbec... tuesday morning
